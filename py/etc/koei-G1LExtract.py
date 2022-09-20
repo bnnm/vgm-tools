37dd730ab44f4048c89d9e07f266e149a74ad752
bnnm <bananaman255@gmail.com>
2022-09-20 19:10:43 +0200

    scripts

--- py/etc/koei-G1LExtract.py
+++ py/etc/koei-G1LExtract.py
@@ -5,10 +5,16 @@
 #   (extract unique entries only, fix KOVS/ATSL, etc)
 # v20210912 by bnnm 
 #   (python3, subdirs)
+# v20220920 by bnnm 
+#   (accept multiple files for easier drag and drop, minor cleanup)
 
 import sys
 import os
 import struct
+
+# config
+subfolders = False  # (make subfolder per file, if file has multiple subfiles only)
+#---
 
 def readu32(file, le=True):
     if le:
@@ -16,129 +22,139 @@ def readu32(file, le=True):
     else:
         return struct.unpack(">I", file.read(4))[0] 
 
-subfolders = True
-if len(sys.argv) != 2:
-   print("Usage: G1LExtract infile.g1l")
-   exit()
+def parse_file(filepath):
+    g1l = open(filepath, 'rb')
+    fourcc = g1l.read(4)
+    if fourcc == b"_L1G":
+        le = True
+    elif fourcc == b"G1L_":
+        le = False
+    else:
+        print("Infile is not a G1L file")
+        exit()
 
-g1l = open(sys.argv[1], 'rb')
-fourcc = g1l.read(4)
-if fourcc == b"_L1G":
-    le = True
-elif fourcc == b"G1L_":
-    le = False
-else:
-    print("Infile is not a G1L file")
-    exit()
+    g1l.seek(0x08)
+    ext = ".bin"
 
-g1l.seek(8)
-ext = ".g1l"
+    fullSize = readu32(g1l, le)
+    headerSize = readu32(g1l, le)
+    codec = readu32(g1l, le)
+    fileCount = readu32(g1l, le)
 
-fullSize = readu32(g1l, le)
-headerSize = readu32(g1l, le)
-codec = readu32(g1l, le)
-fileCount = readu32(g1l, le)
+    offsetsDone = {}
+    filesDone = 0;
 
-offsetsDone = {}
-filesDone = 0;
-
-for i in range(fileCount):
-    g1l.seek(0x18+i*4)
-    #print(hex(g1l.tell()))
-    fileOffset = readu32(g1l, le)
-    
-    # entries can point to the same file multiple times,
-    # simply ignore repeated offsets
-    if hex(fileOffset) in offsetsDone:
-        continue
-    offsetsDone[hex(fileOffset)] = True
-
-    g1l.seek(fileOffset)
-    header = g1l.read(4)
-    if header == "RIFF":
-        fileSize = readu32(g1l) + 8
-        g1l.seek(0x24,1)
-        guid = g1l.read(0x10)
-        if guid == "\xD2\x42\xE1\x47\xBA\x36\x8D\x4D\x88\xFC\x61\x65\x4F\x8C\x83\x6C":
-            ext = ".at9"
-        elif guid == "\xBF\xAA\x23\xE9\x58\xCB\x71\x44\xA1\x19\xFF\xFA\x01\xE4\xCE\x62":
-            ext = ".at3"
-        else:
-            print("Unknown RIFF header with GUID %s" % guid)
-
-    elif header == b"WiiB":
-        g1l.seek(0x18,1)
-        channelSize = readu32(g1l, False)
-        channelCount = readu32(g1l, False)
-        ext = ".dsp" #".wiibgm" #.dsp in Romance of Three Kingdoms 12 WiiU?
-        fileSize = channelCount * channelSize + 0x800
-
-    elif header == b"WiiV":
-        g1l.seek(0xc,1)
-        fileSize = readu32(g1l, False)
-        ext = ".dsp" #".wiivoice" #.dsp in Romance of Three Kingdoms 12 WiiU?
-
-    elif header == b"KTSS":
-        ext = ".ktss"
-        fileSize = readu32(g1l, le)
-
-    elif header == b"ATSL":
-        # .atsl don't have a size, and entries may point again to a previous
-        # subfile (like G1L does), so we can't sum all subfile sizes.
-        # Instead, find final (highest) subfile and use offset+size as file end.
-
-        g1l.seek(fileOffset + 0x04)
-        headerSize = readu32(g1l, le)
-        g1l.seek(fileOffset + 0x0c)
-        subfileType = readu32(g1l)
-        g1l.seek(fileOffset + 0x14)
-        subfileCount = readu32(g1l, le)
+    for i in range(fileCount):
+        g1l.seek(0x18+i*4)
+        #print(hex(g1l.tell()))
+        fileOffset = readu32(g1l, le)
         
-        subfileLe = True
-        if (subfileType & 0x0000FFFF) == 0x0200: # PSP .at3
-            subfileLe = False
+        # entries can point to the same file multiple times,
+        # simply ignore repeated offsets
+        if hex(fileOffset) in offsetsDone:
+            continue
+        offsetsDone[hex(fileOffset)] = True
 
-        entrySize = 0x28
-        if (subfileType & 0x000000FF) == 1:
-            entrySize = 0x3c
+        g1l.seek(fileOffset)
+        header = g1l.read(4)
+        if header == "RIFF":
+            fileSize = readu32(g1l) + 8
+            g1l.seek(0x24,1)
+            guid = g1l.read(0x10)
+            if guid == "\xD2\x42\xE1\x47\xBA\x36\x8D\x4D\x88\xFC\x61\x65\x4F\x8C\x83\x6C":
+                ext = ".at9"
+            elif guid == "\xBF\xAA\x23\xE9\x58\xCB\x71\x44\xA1\x19\xFF\xFA\x01\xE4\xCE\x62":
+                ext = ".at3"
+            else:
+                print("Unknown RIFF header with GUID %s" % guid)
 
-        maxSubfileOffset = 0
-        maxSubfileSize = 0
-        for j in range(subfileCount):
-            g1l.seek(fileOffset + headerSize + j*entrySize + 0x04)
-            subfileOffset = readu32(g1l, subfileLe)
-            g1l.seek(fileOffset + headerSize + j*entrySize + 0x08)
-            subfileSize = readu32(g1l, subfileLe)
+        elif header == b"WiiB":
+            g1l.seek(0x18,1)
+            channelSize = readu32(g1l, False)
+            channelCount = readu32(g1l, False)
+            ext = ".dsp" #".wiibgm" #.dsp in Romance of Three Kingdoms 12 WiiU?
+            fileSize = channelCount * channelSize + 0x800
 
-            if maxSubfileOffset < subfileOffset:
-                maxSubfileOffset = subfileOffset
-                maxSubfileSize = subfileSize
+        elif header == b"WiiV":
+            g1l.seek(0xc,1)
+            fileSize = readu32(g1l, False)
+            ext = ".dsp" #".wiivoice" #.dsp in Romance of Three Kingdoms 12 WiiU?
 
-        ext = ".atsl"
-        fileSize = maxSubfileOffset + maxSubfileSize
+        elif header == b"KTSS":
+            ext = ".ktss"
+            fileSize = readu32(g1l, le)
 
-    elif header == b"KOVS":
-        ext = ".kvs"
-        fileSize = readu32(g1l, le) + 0x20
+        elif header == b"ATSL":
+            # .atsl don't have a size, and entries may point again to a previous
+            # subfile (like G1L does), so we can't sum all subfile sizes.
+            # Instead, find final (highest) subfile and use offset+size as file end.
 
+            g1l.seek(fileOffset + 0x04)
+            headerSize = readu32(g1l, le)
+            g1l.seek(fileOffset + 0x0c)
+            subfileType = readu32(g1l)
+            g1l.seek(fileOffset + 0x14)
+            subfileCount = readu32(g1l, le)
+            
+            subfileLe = True
+            if (subfileType & 0x0000FFFF) == 0x0200: # PSP .at3
+                subfileLe = False
+
+            entrySize = 0x28
+            if (subfileType & 0x000000FF) == 1:
+                entrySize = 0x3c
+
+            maxSubfileOffset = 0
+            maxSubfileSize = 0
+            for j in range(subfileCount):
+                g1l.seek(fileOffset + headerSize + j*entrySize + 0x04)
+                subfileOffset = readu32(g1l, subfileLe)
+                g1l.seek(fileOffset + headerSize + j*entrySize + 0x08)
+                subfileSize = readu32(g1l, subfileLe)
+
+                if maxSubfileOffset < subfileOffset:
+                    maxSubfileOffset = subfileOffset
+                    maxSubfileSize = subfileSize
+
+            ext = ".atsl"
+            fileSize = maxSubfileOffset + maxSubfileSize
+
+        elif header == b"KOVS":
+            ext = ".kvs"
+            fileSize = readu32(g1l, le) + 0x20
+
+        else:
+            print ("Unknown file type %s" % (header))
+            continue
+        
+        g1l.seek(fileOffset)
+        if fileCount == 1:
+            fileName = os.path.splitext(filepath)[0] + ext
+        else:
+            fileName = os.path.splitext(filepath)[0] + "_%04d%s" % (filesDone, ext)
+            if subfolders:
+                folder = os.path.splitext(filepath)[0]
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                fileName = os.path.join(folder, fileName)
+        filesDone += 1
+
+        with open(fileName, 'wb') as f:
+            data = g1l.read(fileSize)
+            f.write(data)
+
+        print("Extracted: %s" % (fileName))
+    g1l.close()
+
+
+def main():
+    argc = len(sys.argv)
+    if argc < 2:
+        print("Usage: G1LExtract infile.g1l (infile.g1l ...)")
+        exit()
     else:
-        print ("Unknown file type %s" % (header))
-        continue
-    
-    g1l.seek(fileOffset)
-    if fileCount == 1:
-        fileName = os.path.splitext(sys.argv[1])[0] + ext
-    else:
-        fileName = os.path.splitext(sys.argv[1])[0] + "_%04d%s" % (filesDone, ext)
-        if subfolders:
-            folder = os.path.splitext(sys.argv[1])[0]
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            fileName = os.path.join(folder, fileName)
-    filesDone += 1
+        for i in range(1, argc):
+            parse_file(sys.argv[i])
 
-    outFile = open(fileName, 'wb')
-    outFile.write(g1l.read(fileSize))
-    outFile.close()
-    print("Wrote %s" % (fileName))
-g1l.close()
+if __name__ == "__main__":
+    main()
--- py/vgmtoolbox-renamer.py
+++ py/vgmtoolbox-renamer.py
@@ -1,41 +1,35 @@
 # renames VGMToolbox's output hex filenames to int 
 # may have some issues
+import os, sys
 
-import re
-import os
-import sys
+#mode = 'p'
 
-mode = 'p'
-
-format = re.compile('(.*[^_])([_])([0-9A-Fa-f]{8})([\.])(.+)')
 rootdir = '.'
 
 for filename in os.listdir(rootdir):
 
-    match = format.match(filename)
-    if not match:
+    basename, ext = os.path.splitext(filename)
+
+    if ext in ['.py']:
         continue
+    if len(basename) < 8:
+        continue
+    
+    
+    start = basename[:-8]
+    str_hex = basename[-8:]
 
-    #group0: full filename
-    #group1: base name
-    #group2: _
-    #group3: hex number
-    #group4: .
-    #group5: extension
-
-    str_hex = match.group(3)
     num_dec = int(str_hex, 16)
     str_dec = str(num_dec).zfill(8)
 
-    if mode == 'p':
-        new_filename = match.group(1) + "." + match.group(5)
-    else:
-        # double __ to avoid clashing with not-yet-renamed files
-        new_filename = match.group(1) + "__" + str_dec + "." + match.group(5)
+    #if mode == 'p':
+    #    new_filename = match.group(1) + "." + match.group(5)
+    #else:
+    # double __ to avoid clashing with not-yet-renamed files
+    new_filename = start + "_" + str_dec + ext
+    print(new_filename)
 
     if os.path.exists(new_filename)==True:
         print("Error: existing file" + new_filename)
         sys.exit(0)
     os.rename(filename, new_filename)
-
-#raw_input('done')
